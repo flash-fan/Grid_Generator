df4acb16e904bbfd0ce4810b78a1c520636bf71a
flash-fan <132087857+flash-fan@users.noreply.github.com>
2023-04-29 19:03:14 +0530

    restructured code base; added support for Horizontal grid creation

--- main.py
+++ main.py
@@ -1,73 +1,150 @@
 import urllib.request
 from PIL import Image
 import tkinter as tk
-import os
-
 from tkinter import filedialog, simpledialog
+import os
+import questionary as qr
 
-root = tk.Tk()
-root.withdraw()
-
-imgs = []
-directory = filedialog.askdirectory(mustexist=True,title="Choose a folder with cards")
-
-for filename in sorted(os.listdir(directory)):
-    if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
-        img = Image.open(os.path.join(directory, filename))
-        imgs.append(img)
-
-# dimensions of the base image
-imgHeight = 800
-imgWidth = 600
-
-# Ask the user for the no. of columns in the grid and automatically calculate the no. of rows.
-while True:
-    num_cols = simpledialog.askinteger("Columns", "Enter the number of columns:")
-    if num_cols is not None and type(num_cols) == int:
-        break
-    else:
-        print("Invalid input. Please enter a whole number.")
-
-num_rows = (len(imgs) + num_cols - 1) // num_cols
+#Constants
+IMG_HEIGHT = 800
+IMG_WIDTH = 600
+BORDER_URL = "https://i.imgur.com/AMBdG9m.png"
+PADDING = 121
+BRDR_HEIGHT = 155
 
 
-borderURL = "https://i.imgur.com/AMBdG9m.png"
-filename = "border.png"
+class Grid:
+    def __init__(self) -> None:
+        self.imgs = []
+        self.root = tk.Tk()
+        self.root.withdraw()
+        self.imgFetch()
 
-with urllib.request.urlopen(borderURL) as response, open(filename, "wb") as out_file:
-    data = response.read()
-    out_file.write(data)
+        # Asking the user for which types of grid they want to generate
+
+        gridTypes = ["Horizontal", "Slanted", "Both"]
+        gridType = qr.select(
+            "Choose which type of grid you want to generate:",
+            choices=gridTypes,
+            default=gridTypes[0],
+        ).ask()
+
+        # Asking the user for the directory in which cards are stored 
+        # + the number of columns in the grid
+        self.dirChoose()
+        self.columnChoose()
+
+        if gridType == gridTypes[0]:
+            print("Plotting Horizontal Grid now...\n")
+            self.horizontalGrid()
+        elif gridType == gridTypes[1]:
+            print("Plotting Vertical Grid now...\n")
+            self.verticalGrid()
+        else:
+            print("Plotting Horizontal Grid now...\n")
+            self.horizontalGrid()
+            print("Plotting Vertical Grid now...\n")
+            self.verticalGrid()
+
+    def dirChoose(self):
+        self.directory = filedialog.askdirectory(title="Choose a Folder with Cards")
+        for filename in sorted(os.listdir(self.directory)):
+            if (
+                filename.endswith(".png")
+                or filename.endswith(".jpg")
+                or filename.endswith(".jpeg")
+            ):
+                img = Image.open(os.path.join(self.directory, filename))
+                self.imgs.append(img)
+
+    def columnChoose(self):
+        self.num_cols = 1
+        while True:
+            self.num_cols = simpledialog.askinteger(
+                "Columns", "Enter the number of columns:"
+            )
+            if self.num_cols is not None and type(self.num_cols) == int:
+                break
+            else:
+                print("Invalid input! Please try again")
+
+        self.num_rows = (len(self.imgs) + self.num_cols - 1) // self.num_cols
+
+    def imgFetch(self):
+        self.borderName = "border.png"
+        with urllib.request.urlopen(BORDER_URL) as response, open(
+            self.borderName, "wb"
+        ) as outFile:
+            data = response.read()
+            outFile.write(data)
+
+    def horizontalGrid(self):
+        gridHeight = IMG_HEIGHT * self.num_rows
+        gridWidth = IMG_WIDTH * self.num_cols
+        grid = Image.new("RGBA", (gridWidth, gridHeight), (255, 0, 0, 0))
+        for i, img in enumerate(self.imgs):
+            rowNum = i // self.num_cols
+            colNum = i % self.num_cols
+            x = IMG_WIDTH * colNum
+            y = IMG_HEIGHT * rowNum
+            try:
+                grid.paste(img, (x, y))
+            except Exception as e:
+                print(f"Failed to place image at row {rowNum + 1}, column {colNum + 1}")
+                exit(-1)
+            else:
+                print("Image {:02d} successfully plotted!".format(i + 1))
+
+        grid.save("Grid-Horizontal.png")
+
+    def verticalGrid(self):
+        gridHeight = (
+            IMG_HEIGHT * self.num_rows + PADDING * (self.num_cols - 1) + BRDR_HEIGHT * 2
+        )
+        gridWidth = IMG_WIDTH * self.num_cols
+        grid = Image.new("RGBA", (gridWidth, gridHeight), (255, 0, 0, 0))
+        border = Image.open(self.borderName)
+
+        # This loop places the white borders on the top and the bottom of the grid
+        for i in range(len(self.imgs)):
+            colNum = i % self.num_cols
+            x = IMG_WIDTH * colNum
+            y = PADDING * (self.num_cols - (colNum + 1))
+            try:
+                grid.paste(border, (x, y))
+            except Exception as e:
+                print(f"Failed to place top border for column {colNum+1}!")
+                input()
+                exit(-1)
+            y = gridHeight - BRDR_HEIGHT - PADDING * (colNum)
+            try:
+                grid.paste(border, (x, y))
+            except Exception as e:
+                print(f"Failed to place bottom border for column {colNum+1}")
+                input()
+                exit(-1)
+
+        # Places the image at their respective positions
+        for i, image in enumerate(self.imgs):
+            rowNum = i // self.num_cols
+            colNum = i % self.num_cols
+            x = IMG_WIDTH * colNum
+            y = (
+                IMG_HEIGHT * rowNum
+                + PADDING * (self.num_cols - colNum - 1)
+                + BRDR_HEIGHT
+            )
+            try:
+                grid.paste(image, (x, y))
+            except Exception as e:
+                print(f"Failed to plot image at row {rowNum+1}, column {colNum + 1}")
+                input()
+                exit(-1)
+            else:
+                print("Image {:02d} successfully plotted!".format(i + 1))
+
+        grid.save("Grid-Vertical.png")
 
 
-border = Image.open(filename)
-
-# Checks if the number of columns is suitable to generate a grid. If not, program is exited automatically
-assert num_cols * num_rows == len(
-    imgs
-), "Number of rows and columns must match the number of images!"
-
-# This accounts for the slant of the grid as well as the white borders which are to be placed on top
-
-grdHght = 800 * num_rows + 121 * (num_cols - 1) + 310
-grdWdth = 600 * num_cols
-
-grid = Image.new("RGBA", (grdWdth, grdHght), (255, 0, 0, 0))
-
-for i in range(len(imgs)):
-    col = i % num_cols
-    x = imgWidth * col
-    y = 121 * (num_cols - (col + 1))
-    grid.paste(border, (x, y))
-    y = grdHght - 155 - 121 * (col)
-    grid.paste(border, (x, y))
-
-for i, img in enumerate(imgs):
-    row = i // num_cols
-    col = i % num_cols
-    x = imgWidth * col
-    y = imgHeight * row + 121 * (num_cols - col - 1) + 155
-    grid.paste(img, (x, y))
-    print(f"Image {i+1} plotted successfully!")
-
-
-grid.save("grid.png")
+if __name__ == "__main__":
+    app = Grid()
